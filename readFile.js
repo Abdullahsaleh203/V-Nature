78d72a3d36f871dba71f2387416c276560508c47
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-01-24 16:14:16 +0200

    Add data.txt, rename package, and create readFile.js for file operations

---
+++ readFile.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+// const data =fs.readFile('data.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+
+// const write = fs.writeFile('data2.txt', data , 'utf8', (err) => { 
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
+// });
