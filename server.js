0dcc0dc7ec0ab84ab5ab99e797396b2c196a22f6
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-01-25 20:29:27 +0200

    Add stream.js for file streaming and update server.js to read data from data.json

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 const express = require('express');
-// const Route = require('express.Router');
-const app = express();
 const fs = require('fs');
+// const path = require('path');
+// const url = require('url');
+// const slugify = require('slugify');
+
+const app = express();
 // app.use(express.json());
+// import url from 'url';
+// import path from 'path';
+
+// const __filename = url.fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__filename);
+// console.log(__dirname);
 
 const PORT = process.env.PORT || 3000;
+
+const read = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
+
 app.get('/test', (req, res) => { 
-    // const read = fs.createReadStream('package.json'); 
-    // read.on('data', chunk => {
-    //     res.write(chunk);
-    // });
-    // read.on('end', () => {
-    //         res.end();
-    //     });
-    //     read.on('error', error=>{
-    //             console.log(error)
-    //             res.end("File not Found !")
-    //         })
-                
-    const read2 = fs.createReadStream('data.txt');
-    read2.pipe(res);
-    
+    res.statusCode.json({data})
+
 });
 
 
