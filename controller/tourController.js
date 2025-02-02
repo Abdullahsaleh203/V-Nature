4593aa5cb765085547850dccf95943907c122d64
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-02-02 15:29:01 +0200

    Refactor updateTour function to improve formatting and maintain consistency

--- controller/tourController.js
+++ controller/tourController.js
@@ -95,11 +95,12 @@ try{
 };
 
 exports.updateTour = async (req, res) => {
-  try{
+  try {
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body,{
       new: true,
       runValidators: true
-    })
+    });
+
     res.status(200).json({
       status: 'success',
       data: {
