3ac7f5cfc982dd8b27b6f2a2c0e3f5c2a4d533ae
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-02-08 17:33:37 +0200

    Refactor tour controller and model for improved query handling and structure

--- models/tourModel.js
+++ models/tourModel.js
@@ -11,10 +11,10 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
       maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
       // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
-    slug: String,
+    // slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration']
@@ -44,18 +44,18 @@ const tourSchema = new mongoose.Schema(
     },
     price: {
       type: Number,
-      required: [true, 'A tour must have a price']
+      required: [true, 'A tour must have a price'],
     },
-    priceDiscount: {
-      type: Number,
-      validate: {
-        validator: function(val) {
-          // this only points to current doc on NEW document creation
-          return val < this.price;
-        },
-        message: 'Discount price ({VALUE}) should be below regular price'
-      }
-    },
+    // priceDiscount: {
+    //   type: Number,
+    //   validate: {
+    //     validator: function(val) {
+    //       // this only points to current doc on NEW document creation
+    //       return val < this.price;
+    //     },
+    //     message: 'Discount price ({VALUE}) should be below regular price'
+    //   }
+    // }
     summary: {
       type: String,
       trim: true,
@@ -75,69 +75,71 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(),
       select: false
     },
-    startDates: [Date],
-    secretTour: {
-      type: Boolean,
-      default: false
-    },
-    startLocation: {
-      // GeoJSON
-      type: {
-        type: String,
-        default: 'Point',
-        enum: ['Point']
-      },
-      coordinates: [Number],
-      address: String,
-      description: String
-    },
-    locations: [
-      {
-        type: {
-          type: String,
-          default: 'Point',
-          enum: ['Point']
-        },
-        coordinates: [Number],
-        address: String,
-        description: String,
-        day: Number
-      }
-    ],
-    guides: [
-      {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User'
-      }
-    ]
-  },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  }
-);
+    startDates: [Date]
+  });
+      // ,
+  //   secretTour: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   startLocation: {
+  //     // GeoJSON
+  //     type: {
+  //       type: String,
+  //       default: 'Point',
+  //       enum: ['Point']
+  //     },
+  //     coordinates: [Number],
+  //     address: String,
+  //     description: String
+  //   },
+  //   locations: [
+  //     {
+  //       type: {
+  //         type: String,
+  //         default: 'Point',
+  //         enum: ['Point']
+  //       },
+  //       coordinates: [Number],
+  //       address: String,
+  //       description: String,
+  //       day: Number
+  //     }
+  //   ],
+  //   guides: [
+  //     {
+  //       type: mongoose.Schema.ObjectId,
+  //       ref: 'User'
+  //     }
+  //   ]
+  // },
+  // {
+  //   toJSON: { virtuals: true },
+  //   toObject: { virtuals: true }
+  //  }
+// );
 
 // tourSchema.index({ price: 1 });
-tourSchema.index({ price: 1, ratingsAverage: -1 });
-tourSchema.index({ slug: 1 });
-tourSchema.index({ startLocation: '2dsphere' });
+// tourSchema.index({ price: 1, ratingsAverage: -1 });
+// tourSchema.index({ slug: 1 });
+// tourSchema.index({ startLocation: '2dsphere' });
 
-tourSchema.virtual('durationWeeks').get(function() {
-  return this.duration / 7;
-});
+// tourSchema.virtual('durationWeeks').get(function() {
+//   return this.duration / 7;
+// });
 
-// Virtual populate
-tourSchema.virtual('reviews', {
-  ref: 'Review',
-  foreignField: 'tour',
-  localField: '_id'
-});
+// // Virtual populate
+// tourSchema.virtual('reviews', {
+//   ref: 'Review',
+//   foreignField: 'tour',
+//   localField: '_id'
+// });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create()
-tourSchema.pre('save', function(next) {
-  this.slug = slugify(this.name, { lower: true });
-  next();
-});
+// tourSchema.pre('save', function(next) {
+//   this.slug = slugify(this.name, { lower: true });
+//   next();
+// });
 
 // tourSchema.pre('save', async function(next) {
 //   const guidesPromises = this.guides.map(async id => await User.findById(id));
@@ -157,26 +159,26 @@ tourSchema.pre('save', function(next) {
 
 // QUERY MIDDLEWARE
 // tourSchema.pre('find', function(next) {
-tourSchema.pre(/^find/, function(next) {
-  this.find({ secretTour: { $ne: true } });
+// tourSchema.pre(/^find/, function(next) {
+//   this.find({ secretTour: { $ne: true } });
 
-  this.start = Date.now();
-  next();
-});
+//   this.start = Date.now();
+//   next();
+// });
 
-tourSchema.pre(/^find/, function(next) {
-  this.populate({
-    path: 'guides',
-    select: '-__v -passwordChangedAt'
-  });
+// tourSchema.pre(/^find/, function(next) {
+//   this.populate({
+//     path: 'guides',
+//     select: '-__v -passwordChangedAt'
+//   });
 
-  next();
-});
+//   next();
+// });
 
-tourSchema.post(/^find/, function(docs, next) {
-  console.log(`Query took ${Date.now() - this.start} milliseconds!`);
-  next();
-});
+// tourSchema.post(/^find/, function(docs, next) {
+//   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+//   next();
+// });
 
 // AGGREGATION MIDDLEWARE
 // tourSchema.pre('aggregate', function(next) {
--- server.js
+++ server.js
@@ -1,33 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
-const app = require('./app');
 const mongoose = require('mongoose');
-
 dotenv.config({ path: './config.env' });
+const app = require('./app');
+
 const DB = process.env.DATABASE_URI;
 mongoose.connect(DB)
     .then((result) => { console.log('connected to db .....') })
     .catch((err) => { console.log(err) });
-
-
-
-
-// const newTour = new Tour({
-//     name: "tour 2",
-//     price: 90
-// })
-// newTour.save()
-//     .then(doc => {
-//         console.log(doc);
-//     })
-//     .catch(err => {
-//         console.log(err);
-//     })
-// Middleware
-// if (process.env.NODE_ENV === 'development') {
-//     app.use(morgan('dev'));
-// }
 
 const PORT = process.env.PORT || 3000;
 
