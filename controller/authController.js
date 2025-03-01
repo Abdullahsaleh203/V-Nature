c6f15a225e8560f4d74a2b603c0f8542df6e67fa
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-01 17:01:07 +0200

    Add comment to restrictTo middleware for clarity on role validation

--- controller/authController.js
+++ controller/authController.js
@@ -89,6 +89,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 })
 exports.restrictTo = (...roles) => {
     return (req, res, next) => {
+        // roles ['admin', 'lead-guide']. role='user'
         if (!roles.includes(req.user.role)) {
             return next(new appError('You do not have permission to perform this action', 403));
         }
