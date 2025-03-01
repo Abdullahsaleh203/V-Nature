2c91ae0c5633c36701a9ad26397535f224a5a687
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-01 17:02:08 +0200

    Add whitespace for improved readability in restrictTo middleware

--- controller/authController.js
+++ controller/authController.js
@@ -93,6 +93,7 @@ exports.restrictTo = (...roles) => {
         if (!roles.includes(req.user.role)) {
             return next(new appError('You do not have permission to perform this action', 403));
         }
+        
         next();
     }
 };
