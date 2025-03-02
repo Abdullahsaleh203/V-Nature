62037452d800f4e275f8df8bef26fc89b1d7f2be
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-03-02 11:37:08 +0200

    Add password reset routes and update password reset token generation method

--- models/userModel.js
+++ models/userModel.js
@@ -76,8 +76,9 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     return false;
     // False means NOT changed
 };
-userSchema.method.createPasswordReset = function () {
+userSchema.method.createPasswordResetToken = function () {
     const resetToken= crypto.randomBytes(32).toString('hex');
+    
     this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
     this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
     return resetToken;
