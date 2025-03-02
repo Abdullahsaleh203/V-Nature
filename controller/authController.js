e42b086d145613788e38cc4845481562f3f24aac
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-02 20:38:43 +0200

    Enable password reset email functionality and update sender email address

--- controller/authController.js
+++ controller/authController.js
@@ -113,18 +113,43 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
     const message = `Forgot your password? Submit a PATCH request with your new password and 
     passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-    // try {
-    //     await new Email(user, resetURL).sendPasswordReset();
-    //     res.status(200).json({
-    //         status: 'success',
-    //         message: 'Token sent to email!'
-    //     });
-    // } catch (err) {
-    //     user.passwordResetToken = undefined;
-    //     user.passwordResetExpires = undefined;
-    //     await user.save({ validateBeforeSave: false });
-    //     return next(new appError('There was an error sending the email. Try again later!', 500));
-    // }
+    try {
+        await new sendEmail(user, resetURL).sendPasswordReset();
+        res.status(200).json({
+            status: 'success',
+            message: 'Token sent to email!'
+        });
+    } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+        return next(new appError('There was an error sending the email. Try again later!', 500));
+    }
 });
 
+
+// exports.resetPassword = asyncHandler(async (req, res, next) => {
+//     // 1) Get user based on the token
+//     const hashedToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+//     const user = await User.findOne({
+//         passwordResetToken: hashedToken,
+//         passwordResetExpires: { $gt: Date.now() }
+//     });
+//     // 2) If token has not expired, and there is user, set the new password
+//     if (!user) {
+//         return next(new appError('Token is invalid or has expired', 400));
+//     }
+//     user.password = req.body.password;
+//     user.passwordConfirm = req.body.passwordConfirm;
+//     user.passwordResetToken = undefined;
+//     user.passwordResetExpires = undefined;
+//     await user.save();
+//     // 3) Update changedPasswordAt property for the user
+// // 4) Log the user in, send JWT
+//     const token = signToken(user._id);
+//     res.status(200).json({
+//         status: 'success',
+//         token
+//     });
+// });
 
--- utils/email.js
+++ utils/email.js
@@ -18,7 +18,7 @@ const sendEmail = async options => {
     })
     // 2) Define the email options
     const mailOptions = {
-        from: 'Admin <admin@dev.com>',
+        from: 'Admin <abdallah.saleh203@yahoo.com>',
         to: options.email,
         subject: options.subject,
         text: options.message
