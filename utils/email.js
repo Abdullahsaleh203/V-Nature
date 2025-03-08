ab37bfed7e02649ebf53bb1b49633093237d4dd9
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-08 19:14:02 +0200

    Refactor password reset flow to send token via email and improve error handling

--- utils/email.js
+++ utils/email.js
@@ -3,12 +3,6 @@ const nodemailer = require('nodemailer');
 const sendEmail = async options => {
     // 1) Create a transporter
     const transporter = nodemailer.createTransport({
-        // host: 'smtp.ethereal.email',
-        // port: 587,
-        // auth: {
-        //     user: 'irwin.runolfsson56@ethereal.email',
-        //     pass: 'ynXrhj3yfuj3AutbTa'
-        // }
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         auth: {
@@ -18,11 +12,28 @@ const sendEmail = async options => {
     })
     // 2) Define the email options
     const mailOptions = {
-        from: 'Admin <abdallah.saleh203@yahoo.com>',
+        from: 'abdallah.saleh@yahoo.com',
         to: options.email,
         subject: options.subject,
         text: options.message,
-        html: "<b>Hello world?</b>",
+        html: `<p><b>Hello</b> to myself <img src="cid:note@example.com"/></p>
+        <p>Here's a nyan cat for you as an embedded attachment:<br/><img src="cid:nyan@example.com"/></p>`,
+
+        // AMP4EMAIL
+        amp: `<!doctype html>
+        <html ⚡4email>
+          <head>
+            <meta charset="utf-8">
+            <style amp4email-boilerplate>body{visibility:hidden}</style>
+            <script async src="https://cdn.ampproject.org/v0.js"></script>
+            <script async custom-element="amp-anim" src="https://cdn.ampproject.org/v0/amp-anim-0.1.js"></script>
+          </head>
+          <body>
+            <p><b>Hello</b> to myself <amp-img src="https://cldup.com/P0b1bUmEet.png" width="16" height="16"/></p>
+            <p>No embedded image attachments in AMP, so here's a linked nyan cat instead:<br/>
+              <amp-anim src="https://cldup.com/D72zpdwI-i.gif" width="500" height="350"/></p>
+          </body>
+        </html>`,
 
     }
     // 3) Actually send the email
