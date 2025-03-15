d22d04bc5d0dd4ff897bd15d448a4c08efe90d20
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-15 17:58:51 +0200

    Add security features with Helmet and rate limiting; update app.js and package files

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 
- express = require('express');
+const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const appError = require('./utils/appError')
 const globalErrorHandler = require('./controller/errorHandel')
 const tourRoute = require('./router/tourRoute');
 const userRouter = require('./router/userRoute');
+const { default: helmet } = require('helmet');
 
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
 // Middleware
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
 
 // app.use(express.static(`${__dirname}/public`));
+
+// Limit request from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many request from this IP, please try again in an hour'
+});
+app.use('/api', limiter);
 
 // Add request time to request object
 app.use((req,res ,next)=>{
