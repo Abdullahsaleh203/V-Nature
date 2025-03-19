52d203ee60ab753df883ab09bbb6c3275a464cca
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-03-19 11:53:26 +0200

    Add guides field to tour model and implement pre-save hook for fetching associated users

--- models/tourModel.js
+++ models/tourModel.js
@@ -104,12 +104,13 @@ const tourSchema = new mongoose.Schema(
         day: Number
       }
     ],
-    
+    guides:Array
   }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true }
 }
 );
+
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
@@ -118,6 +119,10 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 })
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await
+    User.findById(id));
+});
 // tourSchema.pre('save', function(next) {
 //   console.log('Will save document...');
 //   next();
