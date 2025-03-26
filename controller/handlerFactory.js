0f267377ae65cdf21c337c993f6a669f093fa704
Abdallah Saleh <abdallah.saleh203@yahoo.com>
2025-03-26 11:22:54 +0200

    Refactor getAll method in handlerFactory.js to support filtering by tourId and improve response structure. Removed commented-out code for better clarity and organization.

--- controller/handlerFactory.js
+++ controller/handlerFactory.js
@@ -43,37 +43,44 @@ exports.createOne = Model => asyncHandler(async (req, res,next) => {
       })
       
 // Get documents by query
-exports.getAll = (Model, popOptions) => asyncHandler(async (req, res, next) => {
-    let query = Model.find();
-    if (popOptions) query = query.populate(popOptions);
-    const doc = await query;
-    res.status(200).json({
-        status: 'success',
-        results: doc.length,
-        data: {
-            doc
-        }
-    });
+// exports.getAll = (Model, popOptions) => asyncHandler(async (req, res, next) => {
+  //     let query = Model.find();
+  //     if (popOptions) query = query.populate(popOptions);
+  //     const doc = await query;
+  //     res.status(200).json({
+    //         status: 'success',
+    //         results: doc.length,
+    //         data: {
+//             doc
+//         }
+//     });
+// });
+
+// Get documents by query
+exports.getAll = Model=> asyncHandler(async (req, res, next) => {
+ // To allow for nested GET reviews on tour (hack)
+   let filter = {};
+  if (req.params.tourId) filter = { tour: req.params.tourId };
+
+ // EXECUTE QUERY
+const features = new APIFeatures(Model.find(filter), req.query)
+  .filter()
+  .sort()
+  .limitFields()
+  .paginate();
+// const doc = await features.query.explain();
+const doc = await features.query;
+
+// SEND RESPONSE
+res.status(200).json({
+  status: 'success',
+  results: doc.length,
+  data: {
+    data: doc
+  }
+  });
 });
 
-// exports.getAllDoc = (Model, popOptions)=> asyncHandler(async (req, res, next) => {
-//   // EXECUTE QUERY
-//   const features = new APIFeatures(Model.find(), req.query)
-//     .filter()
-//     .sort()
-//     .limitFields()
-//     .paginate();
-// const Model = await features.query;
-
-
-//   res.status(200).json({
-//     status: 'success',
-//     results: Model.length,
-//     data: {
-//       Model
-//     }
-//   });
-// });
 // Get one document by ID
 exports.getOne = (Model, popOptions) => asyncHandler(async (req, res, next) => {
     let query = Model.findById(req.params.id);
