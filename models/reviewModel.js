7be472f63a61a34b1776e377398b6969457af343
abdallah gamal <abdallah.saleh203@yahoo.com>
2025-03-28 17:16:19 +0200

    Refactor reviewModel.js for improved readability and functionality
    - Cleaned up formatting by adding spaces for consistency.
    - Enhanced pre and post hooks for average rating calculations on review updates.
    - Improved code organization within the review schema for better maintainability.

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,48 +1,48 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema(
-{
-	
-review:{
-	type: String,
-	required :[true,"Review can't be empty!"]
-	},
-	rating:{
-		type: Number,
-		min:1,
-		max:5,
-		required: true
-	},
-	createAt:{
-		type:Date,
-		default:Date.now()
-	}, 
-	tour:{
-		type :mongoose.Schema.ObjectId,
-		ref:'Tour',
-		required:[true,'Review must belong to a tour']
-	}, 
-	user:{
-		type :mongoose.Schema.ObjectId,
-		ref:'User',
-		required:[true,'Review must belong to a user']
-	}
+	{
 
-});
+		review: {
+			type: String,
+			required: [true, "Review can't be empty!"]
+		},
+		rating: {
+			type: Number,
+			min: 1,
+			max: 5,
+			required: true
+		},
+		createAt: {
+			type: Date,
+			default: Date.now()
+		},
+		tour: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'Tour',
+			required: [true, 'Review must belong to a tour']
+		},
+		user: {
+			type: mongoose.Schema.ObjectId,
+			ref: 'User',
+			required: [true, 'Review must belong to a user']
+		}
+
+	});
 
 
 // Prevent user from submitting more than one review per tour
 // reviewSchema.index({tour:1,user:1},{unique:true});
 
 // Populate user and tour fields when querying reviews
-reviewSchema.pre(/^find/,function(next){
+reviewSchema.pre(/^find/, function (next) {
 	this.populate({
-		path:'tour',
-		select:'name'
+		path: 'tour',
+		select: 'name'
 	}).populate({
-	path:'user',
-	select:'name photo'
+		path: 'user',
+		select: 'name photo'
 	});
 	next();
 });
@@ -66,12 +66,21 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
 		ratingsQuantity: statics[0].nRating,
 		ratingsAverage: statics[0].avgRating
 	});
+};
 
 reviewSchema.post('save', function () {
 	// this points to current review
 	this.constructor.calcAverageRatings(this.tour);
 });
 
-const Review = mongoose.model('Review',reviewSchema);
+reviewSchema.pre(/^fineOneAnd/, async function (next) { 
+	this.r = await this.findOne();
+	next();
+});
+reviewSchema.post(/^fineOneAnd/, async function () {
+	// this.r = await this.findOne(); // does NOT work here, query has already executed
+	await this.r.constructor.calcAverageRatings(this.r.tour);
+});
+const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
